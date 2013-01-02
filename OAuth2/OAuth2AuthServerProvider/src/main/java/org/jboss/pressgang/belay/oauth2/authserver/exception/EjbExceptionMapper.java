d0911f00df51e35d3db9b24b1f89212d2fa228bb
Katie Miller <katie@codemiller.com>
2013-01-03 09:01:29 +1000

    Added mapper for EJB exceptions

---
+++ OAuth2/OAuth2AuthServerProvider/src/main/java/org/jboss/pressgang/belay/oauth2/authserver/exception/EjbExceptionMapper.java
@@ -0,0 +1,31 @@
+package org.jboss.pressgang.belay.oauth2.authserver.exception;
+
+import org.jboss.pressgang.belay.oauth2.authserver.util.AuthServer;
+
+import javax.ejb.EJBException;
+import javax.inject.Inject;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+import java.util.logging.Logger;
+
+import static org.apache.commons.lang.StringUtils.join;
+
+/**
+ * Exception Mapper to catch EJBExceptions and hide internal error information.
+ *
+ * @author kamiller@redhat.com (Katie Miller)
+ */
+@Provider
+public class EjbExceptionMapper implements ExceptionMapper<EJBException> {
+
+    @Inject
+    @AuthServer
+    private Logger log;
+
+    @Override
+    public Response toResponse(EJBException e) {
+        log.warning("Mapping EJBException to server error: " + e.getMessage() + "\n" + join(e.getStackTrace(), '\n'));
+        return Response.serverError().build();
+    }
+}
